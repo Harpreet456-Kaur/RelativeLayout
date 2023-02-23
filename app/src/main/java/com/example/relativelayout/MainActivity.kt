1ca10451ac7fa6e59182edc3463c0839385737c5
Harpreet456-Kaur <125293917+Harpreet456-Kaur@users.noreply.github.com>
2023-02-23 12:22:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import com.example.relativelayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            if (binding.etName.text.isEmpty()){
+                binding.etName.error="Enter your name"
+            }
+            else if (binding.etPhone.text.isEmpty()){
+                binding.etPhone.error="Enter your Contact"
+            }
+            else if (binding.etPass.text.isEmpty()){
+                binding.etPass.error="Enter your Password"
+            }
+            else if (binding.etConfirm.text.isEmpty()){
+                binding.etConfirm.error="Enter your Confirm Password"
+            }
+            else if (binding.tvGender.text.isEmpty()){
+                binding.tvGender.error="Select Gender"
+            }
+        }
+    }
+}
